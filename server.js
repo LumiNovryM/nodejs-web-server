c5b4b08b81cfd739e9007409c7730cfc4e03926a
LumiNovryM <luminovrymekel@gmail.com>
2023-03-29 23:03:44 +0700

    Method/Verbs Request

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const http = require('http');
- 
 /**
  * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
  * 
  * @param request: objek yang berisikan informasi terkait permintaan
  * @param response: objek yang digunakan untuk menanggapi permintaan
+ *  
  */
+const http = require('http');
+ 
 const requestListener = (request, response) => {
+    // Response Handler
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
-}; 
-
+ 
+    // Request Handler
+    const { method } = request;
+ 
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        response.end('<h1>Hai!</h1>');
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+};
+ 
 const server = http.createServer(requestListener);
-
+ 
 const port = 5000;
 const host = 'localhost';
-
+ 
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada https://${host}:${port}`)
+    console.log(`Server berjalan pada http://${host}:${port}`);
 });
