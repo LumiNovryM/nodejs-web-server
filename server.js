6ac388aa287eb495c5b1b4d73b65eebc79d24505
LumiNovryM <luminovrymekel@gmail.com>
2023-03-30 08:45:56 +0700

    Response Status

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const requestListener = (request, response) => {
  
     if(url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if(url === '/about') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if(method === 'POST') {
             let body = [];
@@ -25,12 +28,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 400;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
